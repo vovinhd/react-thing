72be5f8d5e33eb9eeed08503d6683576b88d2173
vovin <timo.volk@gmail.com>
2019-01-17 15:46:35 +0100

    Feed now displays in a list of cards

--- components/FeedView/FeedWidget.js
+++ components/FeedView/FeedWidget.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import {Body, Button, Container, Content, Fab, Icon, Left, ListItem, Right, Text, Thumbnail} from "native-base";
+import {Container, Content, Fab, Icon, Text} from "native-base";
 import {Query} from "react-apollo";
 import {LOAD_FEED} from "../../network/FeedGql";
 import Expo from "expo";
 import {FlatList, RefreshControl} from "react-native";
-import {defaultAvatar} from "./FeedScreen";
+import {FeedPostWidget} from "./FeedPostWidget";
 
 export default class FeedWidget extends Component {
     constructor(props) {
@@ -15,43 +15,30 @@ export default class FeedWidget extends Component {
     render() {
         return (
             <Container>
-                <Content>
-                    <Query query={LOAD_FEED}>
-                        {({loading, error, data, refetch}) => {
-                            if (loading) return <Expo.AppLoading/>;
-                            if (error) return <Text>`Error ${error.message}`</Text>;
-                            return (
-                                <FlatList refreshControl={
-                                    <RefreshControl
-                                        refreshing={this.state.refreshing}
-                                        onRefresh={() => refetch()}
-                                    />
-                                }
-                                          data={data.posts}
-                                          keyExtractor={(item, index) => item.id.toString()}
-                                          renderItem={({item}) => (
-                                              <ListItem icon key={item.id}
-                                                        onPress={() => this.props.navigation.navigate('PostWidget', {postId: item.id})}>
-                                                  <Left>
-                                                      <Button style={{backgroundColor: "#007AFF"}}>
-                                                          <Thumbnail circular
-                                                                     source={{uri: item.author.avatar ? item.author.avatar.path : defaultAvatar}}/>
-                                                      </Button>
-                                                  </Left>
-                                                  <Body>
-                                                  <Text>{item.title}</Text>
-                                                  </Body>
-                                                  <Right>
-                                                      <Icon active name="arrow-forward"/>
-                                                  </Right>
-                                              </ListItem>
-                                          )
-                                          }
+                <Query query={LOAD_FEED}>
+                    {({loading, error, data, refetch}) => {
+                        if (loading) return <Expo.AppLoading/>;
+                        if (error) return <Text>`Error ${error.message}`</Text>;
+                        return (
+
+                            <Content refreshControl={<RefreshControl
+                                refreshing={this.state.refreshing}
+                                onRefresh={() => refetch()}
+                            />
+                            }>
+                                <FlatList
+                                    data={data.posts}
+                                    keyExtractor={(item, index) => item.id.toString()}
+                                    renderItem={({item}) => (
+                                        <FeedPostWidget post={item}
+                                                        onPress={() => this.props.navigation.navigate('PostWidget', {postId: item.id})}/>
+                                    )}
                                 />
-                            )
-                        }}
-                    </Query>
-                </Content>
+                            </Content>
+                        )
+                    }
+                    }
+                </Query>
                 <Fab style={{backgroundColor: '#5067FF'}}
                      position="bottomRight"
                      onPress={() => this.props.navigation.navigate('NewPostWidget')}>
