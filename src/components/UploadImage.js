63109398c8eb4810be3a0e5b5b3277c4ed6cc341
vovin <timo.volk@gmail.com>
2019-01-28 10:59:37 +0100

    image uploading from iOS should now request permissions correctly

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -4,7 +4,7 @@ import {ReactNativeFile} from 'apollo-upload-client';
 import React from 'react';
 import {Dimensions, Image, ImageBackground, View} from 'react-native';
 import {Button, Icon, Text} from 'native-base';
-import {ImagePicker} from 'expo';
+import {ImagePicker, Permissions} from 'expo';
 
 class UploadImage extends React.Component {
     state = {
@@ -13,9 +13,30 @@ class UploadImage extends React.Component {
         height: null,
         media: null,
         uploading: false,
+        uploadFinished: false,
+        permissionsGranted: false,
     };
 
+
+    async componentDidMount() {
+        const permission = await Permissions.getAsync(Permissions.CAMERA_ROLL);
+        if (permission.status !== 'granted') {
+            const newPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+            this.setState({permissionsGranted: newPermission.status !== 'granted'});
+        } else {
+            this.setState({permissionsGranted: true});
+        }
+    }
+
     uploadImage = async () => {
+        if (!this.state.image) {
+            console.error('cannot upload undefined image');
+            return;
+        }
+        if (!this.state.media) {
+            console.error('image already uploaded');
+            return;
+        }
         this.setState({uploading: true});
         console.log(`[INFO] ${this.constructor.name}: File ${JSON.stringify(this.state.image, null, 2)}, width: ${this.state.width}, heigth: ${this.state.height} uploading`);
 
@@ -30,7 +51,7 @@ class UploadImage extends React.Component {
                 this.setState({uploading: false});
                 console.log(`[INFO] ${this.constructor.name}: File ${JSON.stringify(media, null, 2)} uploaded`);
 
-                this.state.media = media;
+                this.setState({media});
                 if (this.props.onUploadFinished) {
                     this.props.onUploadFinished(media.data.upload);
                 } else {
@@ -57,11 +78,14 @@ class UploadImage extends React.Component {
                 name: `${name}.${ext}`,
             });
             this.setState({image: image, width: result.width, height: result.height});
+            if (this.props.onSelected) {
+                this.props.onSelected(this.state.image);
+            }
         }
     };
 
     reset = () => {
-        this.setState({image: null});
+        this.setState({image: null, media: null});
         if (this.props.onCancel) {
             this.props.onCancel();
         }
@@ -83,16 +107,17 @@ class UploadImage extends React.Component {
             )
         }
         if (image) {
-            const imageAspectRatio = this.state.image.height / this.state.image.width;
+            const imageAspectRatio = this.state.height / this.state.width;
+            console.log(imageAspectRatio, this.state.height, this.state.width);
             const width = (Dimensions.get('window').width);
 
             return (
-                <View style={{flex: 0, alignItems: 'center', justifyContent: 'center'}}>
+                <View style={{flex: 0, alignItems: 'center', justifyContent: 'center', height: 300}}>
                     <View style={{flex: 1, flexDirection: 'row'}}>
                         <Button warning onPress={this.reset}>
                             <Text>Abbrechen</Text>
                         </Button>
-                        <Button info onPress={this.pickImage}>
+                        <Button info disabled={!this.state.permissionsGranted} onPress={this.pickImage}>
                             <Text>Wähle ein anderes Bild</Text>
                         </Button>
                     </View>
